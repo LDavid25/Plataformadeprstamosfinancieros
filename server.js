65f2b282ba02cc7a05eac4a99d7f4628569b7a69
LDavid25 <ldavid2514@gmail.com>
2025-11-07 15:22:55 -0400

    fix: correción de server.js

--- server.js
+++ server.js
@@ -7,8 +7,19 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Manejar todas las rutas para SPA
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'), (err) => {
+    if (err) {
+      console.error('Error al cargar el archivo:', err);
+      res.status(500).send('Error al cargar la aplicación');
+    }
+  });
+});
+
+// Manejador de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('¡Algo salió mal!');
 });
 
 app.listen(port, () => {
